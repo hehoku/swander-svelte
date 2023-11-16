28a3f5592f42b122385ea6ae1f62d98be6339c35
hehoku <hehoku@outlook.com>
2023-11-16 21:52:23 +0800

    feat: Add fetchQuote function to get random quote from API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,23 @@
    * @type {{ message: any; } | null}
    */
   export let error = null;
+
+  async function fetchQuote() {
+    error = null;
+    data = null;
+
+    try {
+      const res = await fetch("https://api.quotable.io/random");
+      if (res.ok) {
+        const quoteData = await res.json();
+        data = { quote: [quoteData] }; // 将获取的引用放入 data 对象中
+      } else {
+        error = { message: "Failed to fetch quote" };
+      }
+    } catch (e) {
+      error = { message: e };
+    }
+  }
 </script>
 
 <svelte:head>
@@ -23,6 +40,14 @@
       <p class="text-2xl font-bold self-end">
         —— {data.quote[0].author}
       </p>
+      <button class="bg-blue-500 px-4 py-2 text-white font-bold rounded-md" on:click={fetchQuote}>
+        Get New Quote
+      </button>
     </div>
+  {:else}
+    <!-- 如果没有数据显示，可以在这里添加一个初始的加载按钮或提示 -->
+    <button class="bg-blue-500 px-4 py-2 text-white font-bold rounded-md" on:click={fetchQuote}>
+      Load Quote
+    </button>
   {/if}
 </section>
