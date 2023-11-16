c78d7fdb1b47ccacf47cffe7bfd69de92fd49bf1
hehoku <hehoku@outlook.com>
2023-11-16 21:10:49 +0800

    chore: add type for data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   /**
-   * @type {any}
+   * @type {{ quote: { _id: string, content: string, author: string, tags: string[], authorSlug: string, length: number, dateAdded: string, dateModified: string }[] } | null}
    */
   export let data;
   /**
@@ -17,11 +17,11 @@
 <section>
   {#if error}
     <p>{error.message}</p>
-  {:else}
+  {:else if data}
     <div class="flex flex-col justify-center items-center gap-10">
       <p class="text-2xl font-bold">{data.quote[0].content}</p>
       <p class="text-2xl font-bold self-end">
-        —— Author {data.quote[0].author}
+        —— {data.quote[0].author}
       </p>
     </div>
   {/if}
